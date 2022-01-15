aaeaf12cd6c8535f252e41c3e3f3f66acacd427c
Benjamin Huo <benjamin@kubesphere.io>
2022-01-15 19:29:39 +0800

    Adjust HTTP func signature for latest functions-framework
    Signed-off-by: Benjamin Huo <benjamin@kubesphere.io>

--- functions/Knative/hello-world-go/hello.go
+++ functions/Knative/hello-world-go/hello.go
@@ -6,6 +6,7 @@ import (
 )
 
 // HelloWorld writes "Hello, World!" to the HTTP response.
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
+func HelloWorld(w http.ResponseWriter, r *http.Request) error {
 	fmt.Fprint(w, "Hello, World!\n")
+	return nil
 }
