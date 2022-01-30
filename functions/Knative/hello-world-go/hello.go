eb9b7e8972c19fe5692e5eaa7ae8682e708c09d3
laminar <fangtian@kubesphere.io>
2022-01-30 15:29:30 +0800

    fix: filter out the first character of the url path
    Signed-off-by: laminar <fangtian@kubesphere.io>

--- functions/Knative/hello-world-go/hello.go
+++ functions/Knative/hello-world-go/hello.go
@@ -6,6 +6,6 @@ import (
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) error {
-	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path)
+	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
 	return nil
 }
